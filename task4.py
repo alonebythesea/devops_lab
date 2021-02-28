bb68c97194901b6e9ac87bcb1f991c70052b0faa
alonebythesea <alonebythesea@gmail.com>
2021-02-28 19:28:27 +0300

    homework2 v0.5 - fix test failing

--- task4.py
+++ task4.py
@@ -2,16 +2,16 @@ num_of_commands = int(input())
 
 result = []
 
-for command in range(num_of_commands + 1):
+for command in range(num_of_commands):
     command = input().split(' ')
     if 'insert' in command:
-        result.insert(int(command[1]), command[2])
+        result.insert(int(command[1]), int(command[2]))
     elif 'print' in command:
         print(result)
     elif 'remove' in command:
-        result.remove(command[1])
+        result.remove(int(command[1]))
     elif 'append' in command:
-        result.append(command[1])
+        result.append(int(command[1]))
     elif 'sort' in command:
         result.sort()
     elif 'pop' in command:
--- task5.py
+++ task5.py
@@ -1,15 +1,15 @@
-def selfDividingNumbers(left, right):
+def selfDividingNumbers(left):
     def self_dividing(n):
         for d in str(n):
             if d == '0' or n % int(d) > 0:
                 return False
         return True
     out = []
-    for n in range(left, right + 1):
+    for n in range(left[0], left[1] + 1):
         if self_dividing(n):
             out.append(n)
     return out
 
 
-left, right = int(input()), int(input())
-print(selfDividingNumbers(left, right))
+left = [int(x) for x in input().split(' ')]
+print(selfDividingNumbers(left))
