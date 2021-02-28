0d796053f2fae7784c99963433841beb6944626b
alonebythesea <alonebythesea@gmail.com>
2021-02-28 19:31:18 +0300

    homework2 v0.5 - fix test failing

--- task5.py
+++ task5.py
@@ -1,15 +1,15 @@
-def selfDividingNumbers(left):
+def selfDividingNumbers(borders):
     def self_dividing(n):
         for d in str(n):
             if d == '0' or n % int(d) > 0:
                 return False
         return True
     out = []
-    for n in range(left[0], left[1] + 1):
+    for n in range(borders[0], borders[1] + 1):
         if self_dividing(n):
             out.append(n)
     return out
 
 
-left = [int(x) for x in input().split(' ')]
-print(selfDividingNumbers(left))
+borders = [int(x) for x in input().split(' ')]
+print(selfDividingNumbers(borders))
