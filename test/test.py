5698bb9fa7a9ecb1cf49b9b279c73dcb52659052
alonebythesea <alonebythesea@gmail.com>
2021-03-02 20:15:29 +0300

    homework6 v0.2 - more tests for a god of tests

--- test/test.py
+++ test/test.py
@@ -27,9 +27,20 @@ def test_json_open():
     nose.tools.ok_(check_state('open'), True)
 
 
+def test_json_trash():
+    nose.tools.ok_(check_state('trash'), False)
+
+
 def test_json_none():
     nose.tools.ok_(check_state(None), True)
 
+
+def test_responce_code():
+    nose.tools.ok_(get_pulls('accepted')[0], 200)
+    nose.tools.ok_(get_pulls('needs work')[0], 200)
+    nose.tools.ok_(get_pulls('open')[0], 200)
+    nose.tools.ok_(get_pulls('closed')[0], 200)
+    nose.tools.ok_(get_pulls(None)[0], 200)
 
 if __name__ == '__main__':
     nose.run()
